e5f9e660e84ff1a77749dc55b56911fb4b3d5c59
Juan Norris <jnorris@machinalis.com>
2013-02-05 21:19:07 -0300

    Modified js code for the widget. PEP8 fixes. Code cleaning.

--- colour_field/fields.py
+++ colour_field/fields.py
@@ -1,38 +1,38 @@
 from django.db import models
 from forms import ColourFormField
-import re
+
 
 class ColourField(models.Field):
     """
     Store a colour as a CSS value (ie, #012345)
-    
+
     We don't subclass models.CharField, as then the django admin would use
     it's AdminCharWidget or whatever, and we want to use our own.
     """
-    
+
     __metaclass__ = models.SubfieldBase
     _south_introspects = True
-    
+
     description = "A colour object"
-    
+
     def to_python(self, value):
-        # assert value[0] == "#" 
+        # assert value[0] == "#"
         # assert re.match('#[0-9a-fA-F]{6}', value)
         return value
-    
+
     def get_prep_value(self, value):
         if not (value and value.strip()) and self.null:
             return None
         return value
-    
+
     def value_to_string(self, value):
         if value:
             return value.strip() or None
-        
+
     def formfield(self, *args, **kwargs):
         defaults = {'form_class': ColourFormField}
         defaults.update(kwargs)
         return super(ColourField, self).formfield(*args, **defaults)
-    
+
     def db_type(self, connection):
         return 'char(7)'
--- colour_field/widgets.py
+++ colour_field/widgets.py
@@ -2,25 +2,35 @@ from django import forms
 from django.conf import settings
 from django.utils.safestring import mark_safe
 
+
 class ColourPickerWidget(forms.TextInput):
+    """
+    Widget to display a Colour Picker with js.
+    """
+
     def render(self, name, value, attrs=None):
         rendered = super(ColourPickerWidget, self).render(name, value, attrs)
-        return rendered + mark_safe(u'''<div id="colourpicker_%(name)s" class="colourpicker"></div>
-                    <script type="text/javascript">
-                    $('#colourpicker_%(name)s').farbtastic('#id_%(name)s');
-                    $('#id_%(name)s').focus(function(){
-                        $('#colourpicker_%(name)s').show();
-                    });
-                    $('#id_%(name)s').blur(function(){
-                        $('#colourpicker_%(name)s').hide();
-                    });
-                    $('#colourpicker_%(name)s').hide();
-                    </script>''' % {'name' : name})
-        
+        return rendered + mark_safe(
+            u"""<div id="colourpicker_%(name)s" class="colourpicker"></div>
+            <script type="text/javascript">
+            $(function () {
+              $('#colourpicker_%(name)s').farbtastic('#id_%(name)s');
+              $('#id_%(name)s').focus(function(){
+              $('#colourpicker_%(name)s').show();
+              });
+              $('#id_%(name)s').blur(function(){
+              $('#colourpicker_%(name)s').hide();
+              });
+              $('#colourpicker_%(name)s').hide();
+            });
+            </script>""" % {'name': name}
+        )
+
     class Media:
         css = {
             'all': (settings.STATIC_URL + 'colour_field/css/farbtastic.css',)
         }
+
         js = (
             settings.STATIC_URL + 'colour_field/js/jquery-1.4.2.js',
             settings.STATIC_URL + 'colour_field/js/farbtastic.js',
