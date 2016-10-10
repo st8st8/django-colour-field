ffa266bf210b4c6ab6189e9a171f3b34ea6e83f5
Steve Bradshaw <steve.bradshaw@mycoracle.com>
2016-10-10 15:19:23 +0100

    Added support for PitPik colour picker

--- colour_field/widgets.py
+++ colour_field/widgets.py
@@ -2,6 +2,18 @@ from django import forms
 from django.conf import settings
 from django.utils.safestring import mark_safe
 
+_NUMERALS = '0123456789abcdefABCDEF'
+_HEXDEC = {v: int(v, 16) for v in (x+y for x in _NUMERALS for y in _NUMERALS)}
+LOWERCASE, UPPERCASE = 'x', 'X'
+
+
+def rgb(triplet):
+    return _HEXDEC[triplet[0:2]], _HEXDEC[triplet[2:4]], _HEXDEC[triplet[4:6]]
+
+
+def triplet(rgb, lettercase=LOWERCASE):
+    return format(rgb[0]<<16 | rgb[1]<<8 | rgb[2], '06'+lettercase)
+
 
 class ColourPickerWidget(forms.TextInput):
     """
@@ -33,4 +45,34 @@ class ColourPickerWidget(forms.TextInput):
 
         js = (
             settings.STATIC_URL + 'colour_field/js/farbtastic.js',
+        )
+        
+        
+class PikPikColourPickerWidget(forms.TextInput):
+    
+    defaults = ["ff0000", "00ff00", "0000ff"]
+    
+    def __init__(self, defaults=None, attrs=None, **kwargs):
+        if defaults:
+            self.defaults = defaults
+        super(PikPikColourPickerWidget, self).__init__(attrs, **kwargs)
+        
+    def render(self, name, value, attrs=None):
+        rendered = super(PikPikColourPickerWidget, self).render(name, value, attrs)
+        rgb_list = [rgb(x) for x in self.defaults]
+        rgbs = ", ".join([' {{"r": "{0}", "g": "{1}", "b": "{2}"}}'.format(x[0], x[1], x[2]) for x in rgb_list])
+        return rendered + mark_safe(
+            u"""<div id="colourpicker_{0}" class="colourpicker"></div>
+            <script type="text/javascript">
+                jsColorPicker("input#id_{0}", {{
+                    memoryColors: [{1}]
+                }});
+            </script>""".format(name, rgbs)
+        )
+
+    class Media:
+        js = (
+            settings.STATIC_URL + 'colour_field/js/colors.js',
+            settings.STATIC_URL + 'colour_field/js/jsColorPicker.min.js',
+            settings.STATIC_URL + 'colour_field/js/jsColor.js',
         )
